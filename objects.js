9b746f726311e29714b0425c32c913ddd7453868
Alba Heidari <albaheidari@gmail.com>
2018-09-06 19:18:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName:"song titles"
 };
 
-function updatePlaylist(playlist,artistName,songTitle){
+function updatePlaylist(obj,key,value){
   return Object.assign({},playlist,{ ['artistName']: "songTitle" });
 
 }
